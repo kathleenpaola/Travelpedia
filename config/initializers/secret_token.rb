de03240e43fce2e531fc9efa22f1ef9bbcf5980d
Kathleen <kathleen.paola@gmail.com>
2014-05-07 22:47:37 +0000

    figuring out which travelpedia to use

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelpedia::Application.config.secret_token = '97122cc45c0e8931aa123620356ede6e944538f03eec4732df2f3bcecd436f583e67ef62d4ca3dbaa1e6f10609f30f07fc462100b0c33244e9f45efbd077d076'
